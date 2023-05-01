bb98d0f5a6bf5a062b80ea996a180ac19cda9c70
Endersson Alonso Mendoza Muñoz <endermendoza12@gmail.com>
2023-05-01 10:41:43 -0600

    Primer componente

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header-wrapper">
+            <h1 className="header-banner">Generador de chistes de Chuck Norris</h1>
+        </div>
+    )
+}
+
+export default Header
