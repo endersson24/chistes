a53dd8ebf8d1aecc07f111901e2cd99a72ff44f8
Endersson Alonso Mendoza Muñoz <endermendoza12@gmail.com>
2023-05-01 12:24:30 -0600

    Metodo get funcionando correctamente

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,23 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import imagen from '../../assets/chuck.jpg';
 
-function Body() {
-    const [categoria, setCategoria] = useState();
-    const [categoriaActual, setCategoriaActual] = useState('');
-    const [chisteActual, setChisteActual] = useState('Click');
+export default function Body() {
+    const [categoria, setCategoria] = useState([])
+    const [categoriaActual, setCategoriaActual] = useState('')
+    const [chisteActual, setChisteActual] = useState('Click that button...')
+
+    const obtenerChiste = async () => {
+        try {
+            let url = 'https://api.chucknorris.io/jokes/random';
+            if (categoriaActual !== "") {
+                url = `https://api.chucknorris.io/jokes/random?categoria=${categoria}`;
+            }
+            const response = await axios.get(url);
+            setChisteActual(response.data.value);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
 
     useEffect(() => {
-        axios.get('https://api.chucknorris.io/jokes/categories').then((res) => {
-            setCategoria(res.data);
-        });
-    }, [chisteActual]);
+        const fetchCategories = async () => {
+            try {
+                const response = await axios.get('https://api.chucknorris.io/jokes/categories');
+                setCategoria(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-
+        fetchCategories();
+    }, []);
 
     return (
-        <div>Body</div>
+        <div className="body-wrapper">
+            <img className="body-img" src={imagen} alt="chuck norris" />
+            <div className="body-joke-area">{chisteActual}</div>
+            <div className="body-button-wrapper">
+                <button className="body-joke-button" onClick={obtenerChiste}>
+                    Get Joke
+                </button>
+            </div>
+            <div className="body-radio-wrapper">
+                {categoria.map((cat) => {
+                    return (
+                        <div className="body-radio-button">
+                            <input
+                                type="radio"
+                                name="radio"
+                                value={cat}
+                                id={cat}
+                                onClick={() => setCategoriaActual(cat)}
+                            />
+                            <label htmlFor={cat}>{cat}</label>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
-
-export default Body
